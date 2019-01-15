5c3f711ca426669feb12e1883b6a574b604a5628
yanochoa <yanochoa@email.arizona.edu>
2019-01-14 20:01:44 -0700

    changed the readme and added form as a component

---
+++ src/components/form/index.js
@@ -0,0 +1,66 @@
+import React from "react";
+import "./_form.scss";
+
+const Form = () => (
+  <div className="inputs">
+    <form
+      name="study-group"
+      method="POST"
+      data-netlify="true"
+      action="/success"
+      data-netlify-honeypot="bot-field"
+    >
+      {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+      <input type="hidden" name="form-name" value="study-group" />
+      <p hidden>
+        <label>
+          Don’t fill this out: <input name="bot-field" />
+        </label>
+      </p>
+      Type of Group:
+      <br />
+      <span className="inputs__radio__cluster">
+        <input type="radio" name="type" value="Study Group" checked />
+        <p>Study</p>
+        <br />
+      </span>
+      <span className="inputs__radio__cluster">
+        <input type="radio" name="type" value="Homework Group" />
+        <p>Homework</p>
+      </span>
+      <p>
+        <label>
+          Group Title:
+          <input type="text" name="title" className="form-control" />
+        </label>
+      </p>
+      <span className="inputs__description">
+        <p>
+          <label>
+            Description:{" "}
+            <textarea name="description" className="form-control" />
+          </label>
+        </p>
+
+        <p>
+          <label>
+            When:
+            <input type="text" name="when" className="form-control" />
+          </label>
+        </p>
+
+        <p>
+          <label>
+            Where:
+            <input type="text" name="where" className="form-control" />
+          </label>
+        </p>
+      </span>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  </div>
+);
+
+export default Form;
---
+++ src/pages/form.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Form from "../components/form";
+import "../../sass/style.scss";
+import "../../sass/form.scss";
+
+const FormPage = props => (
+  <section>
+    <Form />
+  </section>
+);
+
+export default FormPage;
