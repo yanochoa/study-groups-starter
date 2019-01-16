35f88e5ca749129b3214fe62a95fbbe485db8d6e
yanochoa <yanochoa@email.arizona.edu>
2019-01-15 23:12:08 -0700

    commented out form component and page

--- src/pages/form.js
+++ src/pages/form.js
@@ -1,12 +1,12 @@
-import React from "react";
-import Form from "../components/form";
-import "../../sass/style.scss";
-import "../../sass/form.scss";
+//this code is commented out on purpose for demo purposes. If you like to see it functioning, uncomment the file.
+// import React from "react";
+// import Form from "../components/form";
+// import "../../sass/style.scss";
 
-const FormPage = props => (
-  <section>
-    <Form />
-  </section>
-);
+// const FormPage = props => (
+//   <section>
+//     <Form />
+//   </section>
+// );
 
-export default FormPage;
+// export default FormPage;
